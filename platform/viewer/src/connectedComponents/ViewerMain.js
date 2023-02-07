48db024a5474a125a4f3157e2034bf486cba5ea3
Alexandr Borisov <alexandr.borisov@mdw.io>
2023-02-07 10:56:48 +0300

    fix qa

--- platform/viewer/src/connectedComponents/ViewerMain.js
+++ platform/viewer/src/connectedComponents/ViewerMain.js
@@ -212,8 +212,8 @@ class ViewerMain extends Component {
 
     if (displaySet && displaySet.images[0] && displaySet.images[0]._data && displaySet.images[0]._data.metadata
       && displaySet.images[0]._data.metadata.PhotometricInterpretation === 'YBR_FULL') {
-      const error = new Error(`Color mode 'YBR Full' not supported`);
-      const message = `Color mode 'YBR Full' not supported`;
+      const error = new Error(`Color mode 'YBR Full' is not supported`);
+      const message = `Color mode 'YBR Full' is not supported`;
       LoggerService.error({ error, message });
       UINotificationService.show({
         autoClose: false,
