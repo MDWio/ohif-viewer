02b850b5e25b95f0acfba480c56965178d5c4f93
Alexandr Borisov <Alexandr.B@nanox.vision>
2023-06-20 21:03:07 +0300

    final fix for cornerstone-wado-image-loader issue

--- platform/viewer/.webpack/webpack.pwa.js
+++ platform/viewer/.webpack/webpack.pwa.js
@@ -61,7 +61,7 @@ module.exports = (env, argv) => {
       // (Note: script-tag version of the viewer will no longer be supported in OHIF v3)
       alias: {
         'cornerstone-wado-image-loader':
-          'cornerstone-wado-image-loader/dist/dynamic-import/cornerstoneWADOImageLoader.min.js',
+          '@alexandrborisovyg/cornerstone-wado-image-loader/dist/dynamic-import/cornerstoneWADOImageLoader.min.js',
       },
     },
     module: {
@@ -95,7 +95,7 @@ module.exports = (env, argv) => {
         },
         {
           from:
-            '../../../node_modules/cornerstone-wado-image-loader/dist/dynamic-import',
+            '../../../node_modules/@alexandrborisovyg/cornerstone-wado-image-loader/dist/dynamic-import',
           to: DIST_DIR,
         },
       ]),
