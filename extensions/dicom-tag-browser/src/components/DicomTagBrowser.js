8daa5173cf413cf702e37d46b69469c24754039b
Alexandr Borisov <alexandr.borisov@mdw.io>
2025-11-05 17:52:45 +0300

    fix cr

--- extensions/dicom-tag-browser/src/components/DicomTagBrowser.js
+++ extensions/dicom-tag-browser/src/components/DicomTagBrowser.js
@@ -13,6 +13,8 @@ const { nameMap } = DicomMetaDictionary;
 
 const { metadataProvider } = OHIFCornerstone;
 
+const DATE_FORMAT = 'ddd, MMM Do YYYY';
+
 const DicomTagBrowser = ({ displaySets, displaySetInstanceUID }) => {
   const [
     activeDisplaySetInstanceUID,
@@ -48,12 +50,12 @@ const DicomTagBrowser = ({ displaySets, displaySetInstanceUID }) => {
           const dateStr = `${SeriesDate}:${SeriesTime}`.split('.')[0];
           const date = moment(dateStr, 'YYYYMMDD:HHmmss');
           if (date.isValid()) {
-            displayDate = date.format('ddd, MMM Do YYYY');
+            displayDate = date.format(DATE_FORMAT);
           }
         } else {
           const date = moment(SeriesDate, 'YYYYMMDD');
           if (date.isValid()) {
-            displayDate = date.format('ddd, MMM Do YYYY');
+            displayDate = date.format(DATE_FORMAT);
           }
         }
       }
